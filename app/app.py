8160a78f8d1af89e02d66b00119944a7f99d7728
Sandeep Reddy <iambsandeepreddy@gmail.com>
2019-08-08 15:20:42 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,34 @@
-from flask import Flask
+import pandas as pd
+import numpy as np
+import pickle
+
+from flask import Flask, url_for, request
+import json
+
+model_path = '/pickle_model.pkl'
+prd_model = pickle.load(model_path)
+prd_model.summary()
 
 app = Flask(__name__)
+@app.route('/predictest')
+def predicttest():
+    return 'predict API working'
 
-@app.route("/")
-def index():
-  return """
-  <h1>Jenkins Test-v2 in Docker!</h1>
-  <p>It is a Jenkins CD Test...BOOOOOMMMM</p>
-  """
-  
+# main predict code
+@app.route('/predict_res', methods=['POST'])
+def predict_res():
+
+    if request.method == 'POST':
+        text_data = pd.DataFrame(request.json)
+		
+        text_out = prd_model.predict(text_data)
+
+        #Convert df t dict and the to Json
+        text_out_dict = text_out.to_dict(orient='records')
+        text_out_json = json.dumps(text_out_dict, ensure_ascii=False)
+
+        return text_out_json
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
-
+     app.run(host="0.0.0.0", debug=True)
